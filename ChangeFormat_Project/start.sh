7bf8239c786977697550e34f0bf0215cd2c51427
Tony <toninus.lcm@gmail.com>
2016-04-04 18:04:03 +0200

    Completare script start

---
+++ ChangeFormat_Project/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+	MAIN_CLASS_FILE="it.csttech.placeholderpackage.PlaceHolderClass"
+	CLASS_PATH=".;lib/*;bin/classes"
+
+
+	echo "---------------------------------"
+	echo "*** Launching $MAIN_CLASS_FILE $@ ***"
+	echo "---------------------------------"
+	echo " "
+
+
+	java -cp $CLASS_PATH $MAIN_CLASS_FILE $@
+	
+	echo " "
+	echo "---------------------------------"
+	
