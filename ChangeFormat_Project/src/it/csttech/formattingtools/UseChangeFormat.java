873bf7e5e145ec178bf48b94dc54ae71ecdfd426
drago-orsone <alberto_dragoni@hotmail.com>
2016-04-05 16:50:25 +0200

    Version with options in main

--- ChangeFormat_Project/src/it/csttech/formattingtools/UseChangeFormat.java
+++ ChangeFormat_Project/src/it/csttech/formattingtools/UseChangeFormat.java
@@ -1,6 +1,6 @@
 package it.csttech.formattingtools;
 
-import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.*;
 import org.apache.log4j.Logger;
 
 public class UseChangeFormat {
@@ -19,9 +19,8 @@ public class UseChangeFormat {
 
   public static void main(String[] args) {
 
-	MyOptions myOptions = new OptionsChangeFormat();
-	CommandLine cmdLine = myOptions.manageOption(args);
-	if (!myOptions.isCheck()) {
+	CommandLine cmdLine = manageOption(args);
+	if (cmdLine == null) {
 		return;
 	}
 
@@ -33,7 +32,6 @@ public class UseChangeFormat {
 	}else if(cmdLine.getOptionValue(IN_FORMAT_OPT, CSV_FORMAT).equals(FW_FORMAT) && cmdLine.getOptionValue(OUT_FORMAT_OPT, FW_FORMAT).equals(CSV_FORMAT)) {
 		changeFormat = new ChangeFormatFWtoCSV();
 	}else{
-		//System.err.println("Error! Invalid format(s).");
 		log.error("Invalid format(s).");
 		return;
 	}
@@ -41,6 +39,77 @@ public class UseChangeFormat {
 	if(changeFormat != null) {
 		readWrite.execute(cmdLine.getOptionValue(IN_FILE_OPT, DEFAULT_IN_FILE), cmdLine.getOptionValue(OUT_FILE_OPT, DEFAULT_OUT_FILE), changeFormat);
 	}
+
+  }
+
+
+
+  static CommandLine manageOption(String[] args) {
+
+		Option helpOption = Option.builder(HELP_OPT)
+                          .longOpt("help")
+			  .hasArg(false)
+                          .required(false)
+                          .desc("print guide")
+                          .build();
+
+     		Option inFileOption = Option.builder(IN_FILE_OPT)
+			  .argName("input file")
+                          .longOpt("inputFile")
+                          .required(false)
+			  .numberOfArgs(1)
+                          .desc("input file")
+                          .build();
+
+     		Option outFileOption = Option.builder(OUT_FILE_OPT)
+			 .argName("output file")
+                         .longOpt("outputFile")
+                         .numberOfArgs(1)
+                         .required(false)
+                         .desc("output file")
+                         .build();
+
+     		Option inFormatOption = Option.builder(IN_FORMAT_OPT)
+			  .argName("input format")
+                          .longOpt("inputFormat")
+                          .required(false)
+			  .numberOfArgs(1)
+                          .desc("input file format")
+                          .build();
+
+    		Option outFormatOption = Option.builder(OUT_FORMAT_OPT)
+			 .argName("output format")
+                         .longOpt("outputFormat")
+                         .numberOfArgs(1)
+                         .required(false)
+                         .desc("output file format")
+                         .build();
+
+     		Options options = new Options();
+    		options.addOption(helpOption);
+   		options.addOption(inFileOption);
+     		options.addOption(outFileOption);
+     		options.addOption(inFormatOption);
+     		options.addOption(outFormatOption);
+
+     		CommandLine cmdLine = null;
+
+     		try{
+     			CommandLineParser parser = new DefaultParser();
+     			cmdLine = parser.parse(options, args); //throws ParseException. IF statement is skipped if ParseExc is catched.
+
+     			if (cmdLine.hasOption("help")) {
+     			    	HelpFormatter formatter = new HelpFormatter();
+     			    	formatter.printHelp("Change Format", options);
+				cmdLine = null;
+     			}
+
+     		}catch( ParseException pe ){
+			log.error("Invalid option(s).");
+			cmdLine = null;
+		}finally{
+     			return cmdLine;
+     		}
 
   }
 
